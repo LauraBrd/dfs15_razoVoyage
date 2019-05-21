397a036bf17cb21d034a0d806b5e3259a4c734c5
vagrant <vagrant@homestead>
2019-05-21 12:41:35 +0000

    génération de la base de donnée avec la migration

---
+++ razo/database/factories/VoyageFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Voyage;
+use Faker\Generator as Faker;
+
+$factory->define(Voyage::class, function (Faker $faker) {
+    return [
+        'destination' => $faker->country,
+        'description' => $faker->text,
+        'prix' => $faker->numberBetween(100,3000),
+        'titre' => $faker->text
+        // 'image' => $faker->imageUrl($width = 640, $height = 480, 'city')
+    ];
+});
